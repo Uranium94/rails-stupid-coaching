3b897e491ac6db87500a68cd0738bec7dc8a53b5
Max Ellis <uraniumax@gmail.com>
2021-11-08 16:04:15 +0100

    stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,16 @@
 class QuestionsController < ApplicationController
 
   def ask
-
   end
-
 
   def answer
     @info = params[:info]
-    if @info == "I am going to work"
-      @answer = "Great"
+    if @info == 'I am going to work'
+      @answer = 'Great'
     elsif @info.include?('?')
-      @answer = "Silly question, get dressed and go to work!"
-    else @answer = "I don't care, get dressed and go to work!"
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
